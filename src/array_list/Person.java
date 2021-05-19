b11c6780c0b4a8f8d4cac790acb6bb7206fccd55
JoJo Myles <joneshiamyles31@gmail.com>
2021-05-19 10:32:11 -0400

    updated person file

--- src/array_list/Person.java
+++ src/array_list/Person.java
@@ -3,8 +3,35 @@ package array_list;
 public class Person {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		class Person {
+			public String name;
+			public int age;
+			public String gender;
+			public boolean isBreathing;
+			public int walkSpeed = 30;
+			
+			// constructor
+			public Person(String name, int age, String gender) {
+				this.name = name;
+				this.age = age;
+				this.gender = gender;
+			}
+			public void read() {
+				System.out.println(this.name + " is reading the book titled: {given}");
+			}
+			public boolean isStillBreathing() {
+				return isBreathing;
+				
+			}
+			public String speak(String msg) {
+				return this.name + " says: " + msg;
+			}
+			
+			public int walk() {
+				return walkSpeed;
+			}
+			
+		}
 	}
 
 }
