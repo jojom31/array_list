5053f807fffa34849b3e747ff6c813d8159d6208
JoJo Myles <joneshiamyles31@gmail.com>
2021-05-19 10:06:45 -0400

    initial commit

---
+++ src/array_list/List.java
@@ -0,0 +1,20 @@
+package array_list;
+
+import java.util.ArrayList;
+
+
+public class List {
+
+	public static void main(String[] args) {
+		ArrayList<String> cars = new ArrayList<String>();
+		cars.add("Volvo");
+		cars.add("Dodge");
+		cars.add("Ford");
+		cars.add("Toyota");
+		System.out.println(cars.get(1));
+		
+		
+
+	}
+
+}
